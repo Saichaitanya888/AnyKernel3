8badb81931c8b1ec15bcbba87343a77927c1a961
Athi07 <58383546+Athi07@users.noreply.github.com>
2024-12-11 13:03:23 +0530

    Update anykernel.sh
    Update script for bitra

--- anykernel.sh
+++ anykernel.sh
@@ -4,42 +4,36 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=perf by YOGRaJ
-do.devicecheck=1
+kernel.string=Realme GT Neo 2 Phoenix Kernel with
+ KernelSU RUI-Ready Version
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=RMX3371
-device.name2=RE54E4L1
-device.name3=spartan
-device.name4=
-device.name5=
+device.name1=RMX3370
+device.name2=RMX2071
+device.name3=RMX2076
+device.name4=RMX2075
+device.name5=RMX2072
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
-### AnyKernel install
-# begin attributes
-attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
-
-
-## boot shell variables
+# shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=auto;
+is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && attributes;
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+## AnyKernel boot install
+dump_boot;
 
 vbmeta_disable_verification;
 
 write_boot;
-
+## end boot install
