fea2cf0764e5d86a5d8c8cc0ca2b992987f44468
Sai-Chaitanya <139475292+Saichaitanya888@users.noreply.github.com>
2025-03-17 17:57:03 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,15 +4,14 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Realme GT Neo 2 perf Kernel with
- KernelSU RUI-Ready Version
+kernel.string=Realme GT Neo 2/3T  B A N K A I  BY  Sai
 do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=RMX3370
-device.name2=RMX2071
+device.name2=RMX3371
 device.name3=RMX2076
 device.name4=RMX2075
 device.name5=RMX2072
@@ -22,7 +21,7 @@ supported.patchlevels=
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
